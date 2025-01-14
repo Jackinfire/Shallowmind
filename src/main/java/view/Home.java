a47ad2e2eb8a2229e93a24cc4886e08e148e080c
Yuichiro <ym3322@ic.ac.uk>
2025-01-14 01:43:33 +0000

    feat: renamed home page to MOPPU

--- src/main/java/view/Home.java
+++ src/main/java/view/Home.java
@@ -111,7 +111,7 @@ public class Home extends Application {
         // Setting up the Scene
         Scene scene = new Scene(anchorPane);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Patient Editor");
+        primaryStage.setTitle("MOPPU");
         primaryStage.show();
 
         primaryStage.setOnCloseRequest(event -> {
