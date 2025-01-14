efb2f14c3bfefae577a219f4fb88e1609aa0f930
Yuichiro <ym3322@ic.ac.uk>
2025-01-14 00:34:39 +0000

    feat: editor

--- src/main/java/view/editMenu/PatientForm.java
+++
@@ -1,8 +0,0 @@
-package view.editMenu;
-
-import javafx.scene.layout.GridPane;
-
-public interface PatientForm {
-    void addComponents(GridPane gridPane);
-    String getTitle();
-}
--- src/main/java/view/utils/WindowDimensions.java
+++
@@ -1,7 +0,0 @@
-package view.utils;
-
-public class WindowDimensions {
-    public static double windowWidth = 0;
-    public static double windowHeight = 0;
-}
-
