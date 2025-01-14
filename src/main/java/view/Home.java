f0ce64f60b14c82e506f09347f4015446f0d0bbb
JoeMavs <joesrmartin@gmail.com>
2025-01-14 01:13:46 +0000

    fix: fixed bug where LiveMonitor thread keeps running after closing all windows

--- src/main/java/view/Home.java
+++ src/main/java/view/Home.java
@@ -1,6 +1,7 @@
 package view;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
@@ -114,7 +115,12 @@ public class Home extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("Patient Editor");
         primaryStage.show();
-    }
+
+        primaryStage.setOnCloseRequest(event -> {
+            Platform.exit(); // Shut down JavaFX runtime
+            System.exit(0); // Force JVM shutdown to ensure all threads have stopped
+                    });
+                }
 
     /**
      * Opens the Healthcare Processional View by opening the Patient Monitor App
@@ -145,6 +151,7 @@ public class Home extends Application {
             hospitalAdminButton.setDisable(false);
         });
     }
+
 
     public static void main(String[] args) {
         launch(args);
